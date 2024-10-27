22ea7f1a65092352eba43c86dfe47359f2220fde
조현진 <111720377+joe-hy-j@users.noreply.github.com>
2024-10-27 19:47:36 +0900

    1027 비즈니스룸 예약 시스템

---
+++ Assets/Scripts/BoothOwner.cs
@@ -0,0 +1,13 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoothOwner : MonoBehaviourPun
+{
+    public void SetBoothOwnerAs()
+    {
+        photonView.RequestOwnership();
+    }
+}
