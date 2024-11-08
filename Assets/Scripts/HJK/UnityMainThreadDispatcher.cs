9f0b80948ff5c96568a655c1e016b83b9f5d6165
jooonkee <h0ngj00nki@naver.com>
2024-11-08 20:15:02 +0900

    Update UnityMainThreadDispatcher.cs

--- Assets/Scripts/HJK/UnityMainThreadDispatcher.cs
+++ Assets/Scripts/HJK/UnityMainThreadDispatcher.cs
@@ -3,25 +3,9 @@ using System.Collections.Generic;
 using System;
 using System.Threading.Tasks;
 
-public class UnityMainThreadDispatcher : MonoBehaviour
+public class UnityMainThreadDispatcher : Singleton<UnityMainThreadDispatcher>
 {
-    private static UnityMainThreadDispatcher instance;
     private readonly Queue<Action> executionQueue = new Queue<Action>();
-
-    public static UnityMainThreadDispatcher Instance()
-    {
-        if (!instance)
-        {
-            instance = FindObjectOfType<UnityMainThreadDispatcher>();
-            if (!instance)
-            {
-                var go = new GameObject("UnityMainThreadDispatcher");
-                instance = go.AddComponent<UnityMainThreadDispatcher>();
-                DontDestroyOnLoad(go);
-            }
-        }
-        return instance;
-    }
 
     public void Enqueue(Action action)
     {
