4cd70c18444ce4a302ff6d84d0c5427e32a88cc0
조현진 <111720377+joe-hy-j@users.noreply.github.com>
2024-11-08 15:25:32 +0900

    1108 backup

---
+++ Assets/Scripts/CHJ/ObjectLoader.cs
@@ -0,0 +1,51 @@
+﻿using Dummiesman;
+using Siccity.GLTFUtility;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ObjectLoader : MonoBehaviour
+{
+    static GameObject loadedObject;
+
+    public static void ImportObj(string objPath)
+    {
+        if (!File.Exists(objPath))
+        {
+            print("File doesn't exist.");
+        }
+        else
+        {
+            if (loadedObject != null)
+                Destroy(loadedObject);
+            loadedObject = new OBJLoader().Load(objPath);
+            ChangeShaderToLit(loadedObject);
+        }
+    }
+
+    static void ChangeShaderToLit(GameObject obj)
+    {
+        Renderer[] renderers = obj.GetComponentsInChildren<Renderer>();
+        foreach (Renderer renderer in renderers)
+        {
+            foreach (Material material in renderer.materials)
+            {
+                material.shader = Shader.Find("Universal Render Pipeline/Lit");
+            }
+        }
+    }
+
+    public static void ImportGLTFAsync(string filepath)
+    {
+        Importer.ImportGLTFAsync(filepath, new ImportSettings(), OnFinishAsync);
+    }
+
+    static void OnFinishAsync(GameObject result, AnimationClip[] animations)
+    {
+        Debug.Log("Finished importing " + result.name);
+        if (loadedObject != null)
+            Destroy(loadedObject);
+        loadedObject = result;
+    }
+}
--- Assets/Scripts/CHJ/Utility/ImageUtillity.cs
+++ Assets/Scripts/CHJ/Utility/ImageUtillity.cs
@@ -19,4 +19,5 @@ public class ImageUtility : MonoBehaviour
             return null;
         }
     }
+
 }
