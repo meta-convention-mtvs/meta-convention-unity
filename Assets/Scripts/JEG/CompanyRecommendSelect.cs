9973cd485a97039c196c2829959287a9357dd8c7
MadJangE00 <66401908+MadJangE00@users.noreply.github.com>
2024-11-14 23:52:32 +0900

    1114 last commit
    수정 보완

---
+++ Assets/Scripts/JEG/CompanyRecommendSelect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CompanyRecommendSelect : MonoBehaviour
+{
+    public TMP_Text example1;
+    public TMP_Text example2;
+
+    public TMP_InputField requestText;
+
+    public void OnClickSetText1()
+    {
+        print(example1.text);
+        requestText.text = example1.text;
+    }
+
+    public void OnClickSetText2() 
+    { 
+        requestText.text = example2.text;
+    }
+
+
+}
