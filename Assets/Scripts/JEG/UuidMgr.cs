1453dee4fde0d1afef447eac586e97c9ec797651
EugeneJang <jiniixz@naver.com>
2024-11-20 17:14:46 +0900

    1120 multi-language setting
    l

---
+++ Assets/Scripts/JEG/UuidMgr.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UuidMgr : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+// 유저 데이터 저장
+// 유저 데이터에서 기업 이름으로 uuid 할당하기
+// .. 유저는 uuid 가지고만 있으면 될.. 듯?
+// TODO: uuid 할당하는 함수 만들기
+
+
+// 기업 uuid 기반으로 기업 데이터 저장
+// 기업의 부스, 기업 정보 데이터가 있다면 사용하고..
+// 없으면 저장 시키기
+// 경로 변경만 잘하면.. 그대로 이용 할 수 있..는...ㄱ... 아님./. ? 
+// 일단 항목 잘 살리면서 유지 할 것
+
+
+// 트레이닝 정보도.. uuid 기반으로 저장, 접근 해야 함
+
+
+
