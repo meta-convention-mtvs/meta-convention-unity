15f98e26bca130906f09fc5e0d971f3007b408a2
EugeneJang <jiniixz@naver.com>
2024-11-22 23:47:18 +0900

    1122 last last commit AITrainingdata Set

--- Assets/Scripts/JEG/AITrainingUI.cs
+++ Assets/Scripts/JEG/AITrainingUI.cs
@@ -1,18 +1,29 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CHJ;
+using System.Security.Permissions;
+using UnityEngine.UI;
 
 public class AITrainingUI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public InputField aiTrainingData;
+    string path;    
+
     void Start()
     {
-        
+        path = DatabasePath.GetCompanyDataPath(UuidMgr.Instance.currentUserInfo.companyUuid, "ai_training_data");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void OnClickSetAiTrainingData(string path1, string aiData)
+    {
+        path1 = path;
+        aiData = aiTrainingData.text;
+        AsyncDatabase.SetDataToDatabase<string>(path1, aiData);
     }
 }
