8f33e71e2d94838e430450a8b2619e83809e75ad
EugeneJang <jiniixz@naver.com>
2024-11-25 14:11:55 +0900

    1125 AI announce setting

---
+++ Assets/Scripts/JEG/AIAnnounceMgr.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AIAnnounceMgr : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    // 오디오 clip 담아두기 
+    public AudioClip[] announceVoice = new AudioClip[15];
+    // 사용 할 텍스트 담아두기
+    public string[] announceText = new string[15];
+
+    public int idx;
+    public int currentIdx;
+
+    private bool isAudioPlay;
+
+    // 텍스트를 띄울 UI 도 만들어라
+    public GameObject aiBubbleUI;
+    // .. text 만 있으면 되는 걸까? 일단 만들어 둠
+    public Text bubbleText;
+
+    void Start()
+    {
+        isAudioPlay = false;
+
+        idx = 0;
+        currentIdx = 15;
+        announceText[0] = "안녕하세요 언어의 장벽 없이 전 세계 기업 부스를 손 쉽게 탐험하고, 글로벌 비지니스 미팅을 지원하는 메타 컨벤션에 오신 걸 환영해요!";
+        announceText[1] = "원하는 부스를 관람하실 수 이도록, 제가 부스를 추천 해 드릴게요";
+        announceText[2] = "관심사나 원하는 정보를 입력 해보세요!";
+
+        announceText[3] = "Hello and welcome to Meta Convention";
+        announceText[4] = "where you can effortlessly explore global corporate booths without language barriers and connect for business meetings!";
+        announceText[5] = "To help you find the right booths, I’ll recommend options based on your interests or needs";
+        announceText[6] = "Just let me know what you're looking for, and I'll guide you! ";
+
+        announceText[7] = "nǐ hǎo, huānyíng láidào META CONVENTION！ zài zhèlǐ, nǐ keyǐ qīngsōng tànsuǒ quánqiú qǐyè zhǎnwèi, cānjiā shāngwù huìyì qǐng ";
+        announceText[8] = "gàosu wo nǐ gǎn xìngqu de xìnxī，wǒ huì wèi nǐ tuījiàn héshì de zhǎnwèi";
+
+        announceText[9] = "안녕하세요, 당신의 맞춤형 비서입니다. ";
+        announceText[10] = "이제부터 하시는 모든 말씀을 제가 통역해드릴게요 !";
+
+        announceText[11] = "Hello! I’m your personal assistant.";
+        announceText[12] = "From now on, I’ll translate everything you say for you!";
+
+        announceText[13] = "nǐhao ! wǒ shì nǐ de zhùlǐ.";
+        announceText[14] = "cóng xiànzài kāishǐ, nǐ shuō de měi jù huà wǒ doū huì fānyì gei nǐ ! ";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            
+            idx++;
+            idx %= 15;
+        }
+
+        // 시점에 맞춰서 idx 지정하고 , 텍스트 노출 시키고, 사운드 재생하기
+        if (idx != currentIdx)
+        {
+            currentIdx = idx;
+            PlayAnounce(announceVoice, idx);
+        }
+
+        if (audioSource.isPlaying)
+        {
+            isAudioPlay = true;
+            aiBubbleUI.SetActive(true);
+        }
+        else if (!audioSource.isPlaying)
+        {
+            isAudioPlay = false;
+            aiBubbleUI.SetActive(false);
+        }
+
+        // 유저의 언어 정보, 상황
+        // 언어 ko, en, zh
+        // 상황 입장, 번역
+        // 3*2 case
+
+        //LanguageSingleton.Instance.language;
+        //UuidMgr
+        //CashedDataFromDatabase.Instance.playerLanguage;
+
+    }
+
+    public void PlayAnounce(AudioClip[] clips, int idx)
+    {
+        audioSource.Stop();
+
+        audioSource.clip = announceVoice[idx];
+        bubbleText.text = announceText[idx];
+
+        audioSource.Play();
+    }
+}
