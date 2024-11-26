1af35d1e458a1706cbaaf282779d4c5b2aa7c78b
EugeneJang <jiniixz@naver.com>
2024-11-26 14:12:57 +0900

    1126 multi lang setting fin

---
+++ Assets/Scripts/JEG/LocalizationMgr.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Localization.Settings;
+
+public class LanguageChangeButton : MonoBehaviour
+{
+    private void Start()
+    {
+        if (LanguageSingleton.Instance.language == "ko")
+        {
+            SetLanguage("ko");
+        } else if (LanguageSingleton.Instance.language == "en")
+        {
+            SetLanguage("en");
+        } else if (LanguageSingleton.Instance.language == "zh")
+        {
+        SetLanguage("zh-Hans");
+        }
+    }
+
+    private void SetLanguage(string languageCode)
+    {
+        var locale = LocalizationSettings.AvailableLocales.Locales.Find(l => l.Identifier.Code == languageCode);
+        if (locale != null)
+        {
+            LocalizationSettings.SelectedLocale = locale;
+        }
+        else
+        {
+            Debug.LogError($"Locale not found: {languageCode}");
+        }
+    }
+}
