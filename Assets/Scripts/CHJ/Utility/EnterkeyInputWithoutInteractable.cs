62e8433fa48a075c2cdbe207f66235e1d7d73911
조현진 <111720377+joe-hy-j@users.noreply.github.com>
2024-11-27 20:08:44 +0900

    df

---
+++ Assets/Scripts/CHJ/Utility/EnterkeyInputWithoutInteractable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnterkeyInputWithoutInteractable : MonoBehaviour
+{
+    Button button;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (button.interactable && Input.GetKeyDown(KeyCode.Return))
+        {
+            button.onClick?.Invoke();
+        }
+    }
+}
