7c913d572ba7dfcd303de7bd3a8a3bbd55a3e6e2
조현진 <111720377+joe-hy-j@users.noreply.github.com>
2024-11-28 05:53:51 +0900

    Update ObjectLoader.cs

--- Assets/Scripts/CHJ/ObjectLoader.cs
+++ Assets/Scripts/CHJ/ObjectLoader.cs
@@ -39,6 +39,46 @@ public class ObjectLoader : MonoBehaviour
     {
         print("ImportGLTFAsync called");
         AssetLoader.LoadModelFromFile(filepath, OnLoadFinish);
+
+        AssetLoaderOptions _assetLoaderOptions = AssetLoader.CreateDefaultLoaderOptions(false, true);
+        AssetLoader.LoadModelFromFile(filepath, OnLoadFinish, OnMaterialsLoad, OnProgress, OnError, null, _assetLoaderOptions);
+    }
+    /// <summary>
+    /// Called when any error occurs.
+    /// </summary>
+    /// <param name="obj">The contextualized error, containing the original exception and the context passed to the method where the error was thrown.</param>
+    private static void OnError(IContextualizedError obj)
+    {
+        Debug.LogError($"An error occurred while loading your Model: {obj.GetInnerException()}");
     }
 
+    /// <summary>
+    /// Called when the Model loading progress changes.
+    /// </summary>
+    /// <param name="assetLoaderContext">The context used to load the Model.</param>
+    /// <param name="progress">The loading progress.</param>
+    private static void OnProgress(AssetLoaderContext assetLoaderContext, float progress)
+    {
+        Debug.Log($"Loading Model. Progress: {progress:P}");
+    }
+
+    /// <summary>
+    /// Called when the Model (including Textures and Materials) has been fully loaded.
+    /// </summary>
+    /// <remarks>The loaded GameObject is available on the assetLoaderContext.RootGameObject field.</remarks>
+    /// <param name="assetLoaderContext">The context used to load the Model.</param>
+    private static void OnMaterialsLoad(AssetLoaderContext assetLoaderContext)
+    {
+        Debug.Log("Materials loaded. Model fully loaded.");
+    }
+
+    /// <summary>
+    /// Called when the Model Meshes and hierarchy are loaded.
+    /// </summary>
+    /// <remarks>The loaded GameObject is available on the assetLoaderContext.RootGameObject field.</remarks>
+    /// <param name="assetLoaderContext">The context used to load the Model.</param>
+    private void OnLoad(AssetLoaderContext assetLoaderContext)
+    {
+        Debug.Log("Model loaded. Loading materials.");
+    }
 }
