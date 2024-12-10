c5c94483af702a44b5774c36ae54c5e6cc406bf7
조현진 <111720377+joe-hy-j@users.noreply.github.com>
2024-12-10 16:21:28 +0900

    df
    df

---
+++ Assets/SceneFadeIn.cs
@@ -0,0 +1,26 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneFadeIn : MonoBehaviour
+{
+    [SerializeField] Image blackScreen;
+    [SerializeField] float duration = 2.0f;
+
+    void Start()
+    {
+        FadeIn(duration);
+    }
+
+    // Fade In 애니메이션
+    void FadeIn(float seconds)
+    {
+        // 검정색 화면을 서서히 투명하게 만드는 애니메이션
+        blackScreen.DOFade(0f, seconds)
+            .OnStart(() => {
+                blackScreen.color = new Color(0, 0, 0, 1);
+            }).OnComplete(() => Destroy(blackScreen.gameObject));
+    }
+}
